67f9d8000ed76acfd62bd820f598007c6c405053
Kodi Craft <kodi@kdcf.me>
2024-12-01 12:40:09 +0100

    Let it snow

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "NixOS modules for the atproto PDS";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-21.11";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
+  in
+  {
+    overlay = final: prev: {
+      
+    };
+
+    packages = forAllSystems (system: {
+      
+    });
+
+    nixosModules.atprotoPDS = { pkgs, ... }: {
+
+    };
+  };
+}
